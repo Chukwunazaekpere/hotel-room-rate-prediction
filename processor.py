9f63a0609aa15b02753556d213f3f4b495f9a9e7
Chukwunazaekpere <ceo.naza.tech@gmail.com>
2024-12-14 04:05:44 +0100

    processor file update

--- processor.py
+++ processor.py
@@ -13,7 +13,7 @@ def process_DB_data():
     start_time = time()
     room_type_name_dict = {}
     processed_file_name = f"./data/processed_data.csv" # save proceesed records .csv
-    header_names = ["Room Rate", "Month Record", "Number of Star", "Hotel Location", "Number of Nights", "Room-type Name"]
+    header_names = ["Room Rate", "Month Record", "Number of Star", "Hotel Location", "Number of Nights", "Room-type Name"] # model features
     write_processed_file(processed_file_name, header_names) # Write CSV header to processed file
     with omp("for"): # OMP directive for multicore processing
         for count in range(len(file_names)):
@@ -21,7 +21,6 @@ def process_DB_data():
             hotel_location = "Residence area" if count < 4 else "Commercial area"
             file_name = file_names[count]
             reservation_file_name = f"./data/reservations/{file_name}.json"
-            # print("\n\t reservation_file_name: ", reservation_file_name)
             with open(reservation_file_name)as reservation_record: # read reservation files with names as designated by reservation_file_name
                 json_record = json.load(reservation_record) # load file as json
                 # print("\n\t json_record: ", json_record)
@@ -33,15 +32,12 @@ def process_DB_data():
                     room_type_name = room_type_res['room_type_name']
                     room_type_name_dict = room_type_res['new_room_type_name_dict']
                     try: 
-                        data = [record['roomRate'],
-                            record['monthRecord'],
-                            number_of_star,
-                            hotel_location,
-                            record["days"],
-                            room_type_name
-                            ]
+                        data = [
+                            record['roomRate'], record['monthRecord'], number_of_star,
+                            hotel_location, record["days"], room_type_name
+                        ]
                         logging.info(msg=f"\n\t [{time()}]: Writing {record_count+1} record from {file_name} to processed file")
-                        write_processed_file(processed_file_name, data)
+                        write_processed_file(processed_file_name, data) #write processed data to a csv file
                         record_count+=1
                         if record_count > 1000: 
                             logging.info(msg=f"\n\t [{time()}]: End of writing record from {file_name} to processed file")
@@ -68,11 +64,9 @@ def get_room_type_name(room_type_file_name, room_type_id, room_type_name_dict):
 
 
 def write_processed_file(file_name, data):
+    """Write processed data to csv file"""
     with open(file_name, "+a")as new_processed_data:
-        # json.dump(data, new_processed_data, sort_keys=True, indent=4, ensure_ascii=False)
-        print("\n\t data: ", data)
         new_csv = csv.writer(new_processed_data, delimiter=",")
-        # new_processed_data.write(new_data)
         new_csv.writerow(data)
 
 
@@ -96,4 +90,7 @@ def get_room_type_id(record, file_name):
                 if room_db_id['$oid'] == room_id:
                     room_type_id = get_room_type_id(room_record, file_name) # recursive call
     return room_type_id
-process_DB_data()
+
+
+if __name__ == "__main__":
+    process_DB_data()
